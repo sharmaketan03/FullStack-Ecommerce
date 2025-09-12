01f06e45e622cab630b6b735200525fd87e477bc
sharmaketan03 <ketan301024@gmail.com>
2025-09-12 10:50:52 +0530

    deploy the ecommerce

--- Frontent/src/Components/GoogleAuth.jsx
+++ Frontent/src/Components/GoogleAuth.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import {GoogleLogin} from "@react-oauth/google"
 import { instance } from '../axios'
 import { useNavigate } from 'react-router-dom'
+import { UserContext } from './UserContext'
+import { useContext } from 'react-router-dom'
 function GoogleAuth() {
+  const {user,setUser}=useContext(UserContext)
   let navigate=useNavigate()
+
+   
     const handelogin=async(response)=>{
               const googletoken=response.credential
               console.log("googletoken:",googletoken)
@@ -11,7 +16,10 @@ function GoogleAuth() {
            let res=await instance.post('/app/details/auth/google',{token:googletoken})
            console.log(res)
            if(res.status==200){
+            setUser({role:res.data.role})
             navigate('/')
+           }else{
+            setUser({role:''})
            }
         }catch(err){
              console.log('GoogleAuth',err)
